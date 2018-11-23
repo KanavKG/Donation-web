17d3191186954ad587c0179ea5dfbde299bd78c0
KanavG <kanav2garg@gmail.com>
2018-11-23 15:01:41 -0500

    Minor changes

--- app.js
+++ app.js
@@ -30,39 +30,45 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var locations = [
-    {name: "AFD Station 4", city: "Atlanta"},
-    {name: "BOYS & GIRLS CLUB W.W. WOOLFOLK", city: "Atlanta"},
-    {name: "PATHWAY UPPER ROOM CHRISTIAN MINISTRIES", city: "Atlanta"},
-    {name: "PAVILION OF HOPE INC", city: "Scottdale"},
-    {name: "KEEP NORTH FULTON BEAUTIFUL", city: "Decatur"},
-    {name: "D&D CONVENIENCE STORE", city: "Sandy Spring"}
+    // {name: "AFD Station 4", city: "Atlanta"},
+    // {name: "BOYS & GIRLS CLUB W.W. WOOLFOLK", city: "Atlanta"},
+    // {name: "PATHWAY UPPER ROOM CHRISTIAN MINISTRIES", city: "Atlanta"},
+    // {name: "PAVILION OF HOPE INC", city: "Scottdale"},
+    // {name: "KEEP NORTH FULTON BEAUTIFUL", city: "Decatur"},
+    // {name: "D&D CONVENIENCE STORE", city: "Sandy Spring"}
 ]
 
 
 app.get("/", function(req, res) {
-    db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    location: "AFD Station 4",
-    role: "Location Employee"
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
-
 res.render("landing");
 
 });
 
 app.post("/", function(req, res){
-
+    //     db.collection("users").add({
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     location: "AFD Station 4",
+    //     role: "Location Employee"
+    // })
+    // .then(function(docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch(function(error) {
+    //     console.error("Error adding document: ", error);
+    // });
     res.redirect("locationspage");
 })
 app.get("/locationspage", function(req, res){
+    db.collection("locations").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        locations.push(doc.data());
+    });
     res.render("locationspage", {locations: locations});
+ //});
+    // res.render("locationspage", {locations: querySnapshot});
+});
 });
 
 app.get("/locationspage/newlocation", function(req, res){
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyANdsKVA9aRHqJSzQxkTez7-9I-HFI6Vko",
+    authDomain: "donationtracker-2bec2.firebaseapp.com",
+    databaseURL: "https://donationtracker-2bec2.firebaseio.com",
+    projectId: "donationtracker-2bec2",
+    storageBucket: "donationtracker-2bec2.appspot.com",
+    messagingSenderId: "515896993726"
+  };
